ff83b1e937a552619a5c1f3d3d040e0ec9c066ef
TahaHussain121 <tahahussain121b@gmail.com>
2023-11-13 14:04:41 +0100

    unit tests for function generate random number and operator

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -4,25 +4,32 @@ from math_quiz import function_A, function_B, function_C
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_InRange(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = test_generate_random_InRange(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_operator(self):
+        # Test the function multiple times to ensure randomness
+        for _ in range(10):
+            operator = generate_random_operator()
 
-    def test_function_C(self):
+            # Check if the generated operator is one of the expected values
+            self.assertIn(operator, ['+', '-', '*'])
+
+
+    def test_math_quiz(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
+                (5.5, -8, '-', '5.5--8', -2.5),
                 ''' TODO add more test cases here '''
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
+                evaluate_expression
                 # TODO
                 pass
 
